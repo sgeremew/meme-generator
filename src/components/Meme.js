6d7a94db2b8832ba03c7ce4e06f9fb64f5dd948d
sgeremew <geremews@yahoo.com>
2023-11-21 19:48:45 -0500

    get random meme from memesData

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,10 +1,23 @@
 import React from "react";
+import memesData from "../memesData";
 
 export default function Meme() {
 
-    function handleButtonClick() {
+    const memes = memesData.data.memes;
+    const memesCount = memes?.length || 0;
+    
+
+    function getNewMemeImg() {
         console.log('Get new meme image!');
-        
+        const memeIndex = getRandomInt(memesCount);
+        console.log('index ', memeIndex);
+        console.log('url ', memes[memeIndex].url);
+    }
+
+    // random int between 0 and max (exclusive)
+    function getRandomInt(max) {
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max));
     }
 
     return (
@@ -20,7 +33,7 @@ export default function Meme() {
                     className="form--input"
                     placeholder="Bottom text"
                 />
-                <button className="form--button" onClick={handleButtonClick}>
+                <button className="form--button" onClick={getNewMemeImg}>
                     Get a new meme image 🖼
                 </button>
             </div>
